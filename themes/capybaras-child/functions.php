dfb0172757a3805d52634c8c29337e62d7a78b12
Adam Grant <adam.grant2@dcmail.ca>
2023-11-01 20:09:13 -0400

    Created child theme with screenshot

---
+++ themes/capybaras-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+ /**
+ * Enqueues scripts and styles.
+ *
+ * @since Twenty Twenty-One 1.0
+ *
+ * @global bool       $is_IE
+ * @global WP_Scripts $wp_scripts
+ *
+ * @return void
+ */
+function capybaras_child_scripts() {
+	wp_enqueue_style( 'capybaras-child', get_stylesheet_uri(), array('twenty-twenty-one-style'), wp_get_theme()->get( 'Version' ) );
+	
+}
+add_action( 'wp_enqueue_scripts', 'capybaras_child_scripts' );
