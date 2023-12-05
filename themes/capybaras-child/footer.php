dc725607f4006b32f0dfe4e1d36fbd0ea4e9c339
Adam Grant <adam.grant2@dcmail.ca>
2023-12-04 20:15:24 -0500

    Edited about template and added a wp query to the footer

---
+++ themes/capybaras-child/footer.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+<?php
+    if ( is_home() || is_page( 'about' ) ) {
+ 
+ $args = array(
+     'post_type'      => 'post',
+     'posts_per_page' => 3,
+     'post_status'    => 'publish',
+     'orderby'        => 'date',
+ );
+
+ $posts_query = new WP_Query( $args );
+
+ if ( $posts_query->have_posts() ) :
+     while ( $posts_query->have_posts() ) : $posts_query->the_post();
+         ?>
+         <article style="margin-left: 40px; margin-right: 40px;"class="thePosts"<?php post_class(); ?>>
+             <h2><?php the_title(); ?></h2>
+             <?php
+             if ( has_post_thumbnail() ) {
+                 the_post_thumbnail( '100px' ); 
+             }
+             ?>
+             <div class="excerpt"><?php the_excerpt(); ?></div>
+             <p><a href="<?php the_permalink(); ?>">Read more</a></p>
+         </article>
+         <?php
+     endwhile;
+     wp_reset_postdata();
+ else :
+     echo 'There are no posts';
+ endif;
+}
+?>
+
+	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+
+	<footer id="colophon" class="site-footer">
+
+		<?php if ( has_nav_menu( 'footer' ) ) : ?>
+			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
+				<ul class="footer-navigation-wrapper">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'items_wrap'     => '%3$s',
+							'container'      => false,
+							'depth'          => 1,
+							'link_before'    => '<span>',
+							'link_after'     => '</span>',
+							'fallback_cb'    => false,
+						)
+					);
+					?>
+				</ul><!-- .footer-navigation-wrapper -->
+			</nav><!-- .footer-navigation -->
+		<?php endif; ?>
+		<div class="site-info">
+			<div class="site-name">
+				<?php if ( has_custom_logo() ) : ?>
+					<div class="site-logo"><?php the_custom_logo(); ?></div>
+				<?php else : ?>
+					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
+						<?php if ( is_front_page() && ! is_paged() ) : ?>
+							<?php bloginfo( 'name' ); ?>
+						<?php else : ?>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						<?php endif; ?>
+					<?php endif; ?>
+				<?php endif; ?>
+			</div><!-- .site-name -->
+
+			<?php
+			if ( function_exists( 'the_privacy_policy_link' ) ) {
+				the_privacy_policy_link( '<div class="privacy-policy">', '</div>' );
+			}
+			?>
+
+			<div class="powered-by">
+				<?php
+				printf(
+					/* translators: %s: WordPress. */
+					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
+					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
+				);
+				?>
+			</div><!-- .powered-by -->
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
